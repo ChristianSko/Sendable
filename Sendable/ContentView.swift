55697a5ad4305311146562f8fd177bcbf89d919e
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-01 13:33:42 +0200

    Initial Commit

---
+++ Sendable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sendable
+//
+//  Created by Skorobogatow, Christian on 1/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sendable/SendableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendableApp.swift
+//  Sendable
+//
+//  Created by Skorobogatow, Christian on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SendableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
